b7b8f29cea99778b6073877f2ded3326de9ff0c7
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2022-12-26 01:12:26 +0100

    Controll sends 2 degrees lower temp than shown

--- src/Lvp.svelte
+++ src/Lvp.svelte
@@ -1,5 +1,5 @@
 <script>
-
+  //Kontrollen visar alltid 2 grader hogre an vad den skickar som kommando, tex star det 20 grader pa kontrollen skickar den 18 grader i meddelandet. Lagsta temp pa kontrollen ar 18 grader och hogsta ar 32.
   let currentTemp = '';
   let temporaryTemp = 20;
   export let irBlasterUrl = 'http://unknown.url';
@@ -16,7 +16,7 @@
     let tempToUse = temp ? temp : 20;
     currentTemp = turnOrKeepOn ? tempToUse : currentTemp;
 
-    return 'IRhvac {"Vendor":"SHARP_AC","Model":3,"Mode":"Heat","Power":"' + power + '","Celsius":"On","Temp":' + tempToUse + ',"FanSpeed":"Auto","SwingV":"Off","SwingH":"Off","Quiet":"Off","Turbo":"Off","Econo":"Off","Light":"Off","Filter":"On","Clean":"Off","Beep":"Off","Sleep":-1}'
+    return 'IRhvac {"Vendor":"SHARP_AC","Model":3,"Mode":"Heat","Power":"' + power + '","Celsius":"On","Temp":' + (tempToUse - 2) + ',"FanSpeed":"Auto","SwingV":"Off","SwingH":"Off","Quiet":"Off","Turbo":"Off","Econo":"Off","Light":"Off","Filter":"On","Clean":"Off","Beep":"Off","Sleep":-1}'
   }
 </script>
 
@@ -24,8 +24,8 @@
   <div>Current temp: {currentTemp}</div>
   <div>
     <label>
-      <input type=number bind:value={temporaryTemp} min=18 max=26>
-      <input type=range bind:value={temporaryTemp} min=18 max=26>
+      <input type=number bind:value={temporaryTemp} min=18 max=32>
+      <input type=range bind:value={temporaryTemp} min=18 max=32>
     </label>
   </div>
   <div>
